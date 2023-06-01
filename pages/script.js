32e582839fc026a4ee0b198933805acb8e6cc892
VeraDgt <veradgt@gmail.com>
2023-06-01 16:38:11 +0300

    feat: form handle complete

--- pages/script.js
+++ pages/script.js
@@ -7,6 +7,7 @@ const popupAddCard = document.querySelector('#popup_add-card');
 const popupImage = document.querySelector('#popup_image');
 const avatarEditButton = profile.querySelector('.profile__avatar-edit-button');
 const popupEditAvatar = document.querySelector('#popup_edit-avatar');
+const popupList = Array.from(document.querySelectorAll('.popup'));
 const formEditProfile = document.forms.profile;
 const formEditAvatar = document.forms.avatar;
 const cardAddForm = document.forms.place;
@@ -20,20 +21,25 @@ const newAvatar = formEditAvatar.avatar;
 
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('form__input_error');
   errorElement.textContent = errorMessage;
   errorElement.classList.add('form__error_active');
+  inputElement.classList.add('form__item_error');
 };
 
   const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove('form__input_error');
   errorElement.classList.remove('form__error_active');
-  errorElement.textContent = '';
+  errorElement.textContent = ''; 
+  inputElement.classList.remove('form__item_error');
 };
 
 const isValid = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
+  if (inputElement.validity.patternMismatch) {
+inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+} else {
+inputElement.setCustomValidity("");
+} if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
     hideInputError(formElement, inputElement);
@@ -42,9 +48,12 @@ const isValid = (formElement, inputElement) => {
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.form__item'));
+  const buttonElement = formElement.querySelector('.form__button');
+  toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
       isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 };
@@ -52,11 +61,30 @@ const setEventListeners = (formElement) => {
 const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll('.form'));
   formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
     setEventListeners(formElement);
   });
 };
 
-enableValidation(); 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}; 
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+        buttonElement.disabled = true;
+    buttonElement.classList.add('form__button_disabled');
+  } else {
+        buttonElement.disabled = false;
+    buttonElement.classList.remove('form__button_disabled');
+  }
+}; 
+
+enableValidation();
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -68,7 +96,6 @@ profileEditButton.addEventListener('click', function() {
   openPopup(popupEditProfile);
 });
 
-// закрытие попапа
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 };
@@ -87,12 +114,13 @@ formEditProfile.addEventListener('submit', handleEditProfileFormSubmit);
 
 // обработка submit
 function handleEditProfileFormSubmit(evt) {
+  const button = evt.target.querySelector('.form__button');
   evt.preventDefault(); 
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
   closePopup(popupEditProfile);
   evt.target.reset();
-  setSubmitButtonState(false);
+  setSubmitButtonState(false, button);
   };
 
 // добавление карточки
@@ -138,13 +166,14 @@ function addCard(newCard) {
 
 function handleAddCardFormSubmit(evt) {
   evt.preventDefault(); 
+  const button = evt.target.querySelector('.form__button');
   const newPlaceImageValue = newPlaceImage.value;
   const newPlaceTitleValue = newPlaceTitle.value;
   const newCard = createCard(newPlaceImageValue, newPlaceTitleValue);
   addCard(newCard);
   closePopup(popupAddCard);
   evt.target.reset();
-  setSubmitButtonState(false);
+  setSubmitButtonState(false, button);
   };
 
 //загрузка карточек
@@ -156,8 +185,6 @@ initialCards.forEach(function (el) {
 
 //смена аватара
 const profileAvatar = profile.querySelector('.profile__avatar');
-console.log(profileAvatar.style.backgroundImage);
-
 
 avatarEditButton.addEventListener('click', function() {
   openPopup(popupEditAvatar);
@@ -167,128 +194,44 @@ formEditAvatar.addEventListener('submit', handleEditAvatarFormSubmit);
 
 function handleEditAvatarFormSubmit(evt) {
   evt.preventDefault(); 
+  const button = evt.target.querySelector('.form__button');
   const newAvatarLink = newAvatar.value;
   profileAvatar.style.backgroundImage = `url(${newAvatarLink})`;
   closePopup(popupEditAvatar);
   evt.target.reset();
-  setSubmitButtonState(false);
+  evt.target
+  setSubmitButtonState(false, button);
   };
 
 function keyHandler(evt) {
   if (evt.key == 'Escape') {
-    closePopup(popupEditProfile);
-    closePopup(popupAddCard);
-    closePopup(popupImage);
-    closePopup(popupEditAvatar);
-    } 
-    // if (evt.target.classList.contains('form__item')) {
-    //   errorHandler;
-    // };
+    popupList.forEach((popupElement) => {
+      closePopup(popupElement);
+    });
+  };
 };
 
 function clickOnOverlayHandler(evt) {
-  if (evt.target.classList.contains('form') || evt.target.closest('.form')) {
+  if (evt.target.classList.contains('form') || evt.target.closest('.form') || evt.target.closest('.popup__image-wrapper')) {
     evt.stopImmediatePropagation();
   }
   else if (evt.target.closest('.popup_opened')) {
-    closePopup(popupEditProfile);
-    closePopup(popupAddCard);
-    closePopup(popupImage);
-    closePopup(popupEditAvatar);
+    popupList.forEach((popupElement) => {
+      closePopup(popupElement);
+    });
   }
 }
 
 document.addEventListener('keydown', keyHandler);
 document.addEventListener('click', clickOnOverlayHandler);
 
-//валидация форм
 
-
-function setSubmitButtonState(isFormValid) {
-  if (isFormValid) {submitButton.removeAttribute('disabled');
+function setSubmitButtonState(isValid, submitButton) {
+  if (isValid) {
+    submitButton.disabled = false;
   submitButton.classList.remove('form__button_disabled'); 
   } else {
-    submitButton.setAttribute('disabled', true);
+    submitButton.disabled = true;
     submitButton.classList.add('form__button_disabled'); 
-  }
-}
-
-function errorHandler(evt) {
-  const isValid = 40 > nameInput.value.length > 0 && 200 > jobInput.value.length > 0 || 30 > newPlaceTitle.value.length > 0;
-  const item = evt.target;
-  const submitButton = Array
-    .from(item.closest('.form__input-container').children)
-    .filter(item => item.classList.contains('form__button'));
-  const error = Array
-  .from(item.closest('.form__input-container').children)
-  .filter(item => item.classList.contains('form__error'));
-  const valueLength = item.value.length;
-  const itemName = item.name;
-  const value = item.value;
-//*** */
-  //console.log(value.match(/^[a-zA-Zа-яА-я -]+$/i));
-//*** */
-
-  //console.log(evt.target.value.length);
-  //console.log(evt.target.value);
-  // console.log(evt.target.name);
-  // console.log(evt.key);
-  // console.log(error[0].classList);
-
-  if (valueLength <= 1) {
-    const textContent = 'Вы пропустили это поле.';
-    if (itemName == 'name') {
-      item.classList.add('form__item_error');
-      error[0].classList.add('form__error_active');
-      error[0].textContent = textContent;
-    } if (itemName == 'user-description') {
-      item.classList.add('form__item_error')
-      error[1].classList.add('form__error_active');
-      error[1].textContent = textContent;
-    } if (itemName == 'card-title' && valueLength == 1) {
-      item.classList.add('form__item_error')
-      error[0].classList.add('form__error_active');
-      error[0].textContent = textContent;
-    }
-    } 
-  //   else if (valueLength > 30) {
-  //   const textContent = 'Превышено допустимое количество символов.';
-  //   if (itemName == 'card-title') {
-  //     error[0].classList.add('form__error_active');
-  //     error[0].textContent = textContent;
-  //   } if (itemName == 'user-name' && valueLength > 40) {
-  //     error[0].classList.add('form__error_active');
-  //     error[0].textContent = textContent;
-  //   } if (itemName == 'user-description' && valueLength > 200)
-  //   error[1].classList.add('form__error_active');
-  //   error[1].textContent = textContent;
-  // } else if (valueLength < 3) {
-  //   const textContent = 'Минимальное количество символов: 2. Длина текста сейчас: 1 символ.';
-  //   if (itemName == 'user-name') {
-  //     error[0].classList.add('form__error_active');
-  //     error[0].textContent = textContent;
-  //   } if (itemName == 'user-description') {
-  //     error[1].classList.add('form__error_active');
-  //     error[1].textContent = textContent;
-  //   } if (itemName == 'card-title' && valueLength == 1) {
-  //     error[0].classList.add('form__error_active');
-  //     error[0].textContent = textContent; 
-  //   }
-  //   }
-  // else if (value.match(/^[a-zA-Zа-яА-я -]+$/i) && itemName == 'card-title' || itemName == 'user-description' || itemName == 'user-name') {
-  //   const textContent = 'Введен некорректный символ.';
-  // }
-  
-  // else {/[\s+,!?]/g
-  //   error[0].classList.remove('form__error_active');
-  //   error[0].textContent = '';
-  //   error[1].classList.remove('form__error_active');
-  //   error[1].textContent = '';
-  // }
-
-
-// if (evt.target.length == 1) {
-//     error.textContent = 'Минимальное количество символов: 2. Длина текста сейчас: 1 символ.';
-//     error.classList.add('form__error_active');
-    setSubmitButtonState(isValid);
-  }
+  };
+};
